75b9e810fcd4740479754d70bd16be693a916d29
Niels Lange <info@nielslange.de>
2020-03-26 21:34:10 +0700

    #71: Content: Show full content on single pages

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ get_header();
 			while ( have_posts() ) {
 				the_post();
 
-				if ( is_single() ) {
-					get_template_part( 'template-parts/content', get_post_type() );
+				if ( is_single() || is_page() ) {
+					get_template_part( 'template-parts/content-single', get_post_type() );
 				} else {
 					get_template_part( 'template-parts/content', get_post_type() );
 				}
