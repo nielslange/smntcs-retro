77d1c6ec01319983d4e809373f83b855dbc65042
Niels Lange <info@nielslange.de>
2020-03-29 18:50:07 +0700

    #74: Fix Travis issue

--- functions.php
+++ functions.php
@@ -193,8 +193,9 @@ function smntcs_retro_skip_link() {
 add_action( 'wp_body_open', 'smntcs_retro_skip_link', 5 );
 
 /**
- * Add Theme Options to the WordPress Customizer
+ * Add theme options to the WordPress customizer
  *
+ * @since 1.6.0
  * @param WP_Customize_Manager $wp_customize The customizer object.
  * @return void
  */
@@ -226,9 +227,15 @@ function smntcs_retro_customize_register( $wp_customize ) {
 }
 add_action( 'customize_register', 'smntcs_retro_customize_register' );
 
+/**
+ * Add custom CSS to the site
+ *
+ * @since 1.6.0
+ * @return void
+ */
 function smntcs_retro_wp_head() {
 	if ( get_theme_mod( 'smntcs_retro_centre_site' ) ) {
 		print( '<style type="text/css">body { margin: auto; }</style>' );
 	}
 }
-add_action( 'wp_head', 'smntcs_retro_wp_head');
+add_action( 'wp_head', 'smntcs_retro_wp_head' );
